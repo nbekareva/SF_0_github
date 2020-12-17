0e0027eded2c847dc7aaff93d8b82a5d0d6c3049
Anastasia Walrave <nbekareva@gmail.com>
2020-12-17 23:58:18 +0700

    async parsing

--- module_3/data_scraping.py
+++ module_3/data_scraping.py
@@ -4,16 +4,16 @@ import asyncio
 import aiohttp
 
 
-urls = pd.read_csv('main_task.csv', usecols=['URL_TA'], squeeze=True)
-urls = urls.iloc[:10000]
+urls = pd.read_csv('main_task.csv', usecols=['Restaurant_id', 'URL_TA'])
+urls = urls
 
-parsed_data = pd.DataFrame(columns=['Cuisine Style', 'Price Range', 'Number of Reviews'])
+parsed_data = pd.DataFrame(columns=['Restaurant_id', 'Cuisine Style', 'Price Range', 'Number of Reviews'])
 
 semaphore = asyncio.Queue(maxsize=1000)
 
 
-async def parse_from_TA(idx, url):
-    print(idx, url, 'test')
+async def parse_from_TA(idx, rest_id, url):
+    print(idx, rest_id, url)
     endpoint = f'https://www.tripadvisor.com{url}'
 
     async with aiohttp.ClientSession() as session:
@@ -38,21 +38,24 @@ async def parse_from_TA(idx, url):
                 smart_strings=False)  # number of reviews
             current_num = int(current_num[0].replace(',', '')) if len(current_num) > 0 else None
 
-    parsed_data.loc[idx, ['Cuisine Style', 'Price Range', 'Number of Reviews']] = [
-        current_cuisines or None, current_price_range, current_num
+    parsed_data.loc[idx, ['Restaurant_id', 'Cuisine Style', 'Price Range', 'Number of Reviews']] = [
+        rest_id, current_cuisines or None, current_price_range, current_num
     ]
     await semaphore.get()
 
 
-async def main(loop):
-    for idx, url in urls.items():
-        await semaphore.put(idx)  # It doesn't matter what we put in the queue. We use it as semaphore.
-        loop.create_task(parse_from_TA(idx, url))  # all the tasks are scheduled at the moment but not all done
+async def main(loop, start=0, end=urls.shape[0]):
+    for idx, row in urls.iloc[start:end].iterrows():
+        if not idx % 5000 and idx > 0:
+            await asyncio.sleep(300)    # make 5 min pauses after each 5000 requests to avoid being banned
+        await semaphore.put(idx)    # It doesn't matter what we put in the queue. We use it as semaphore.
+        rest_id = row['Restaurant_id']
+        url = row['URL_TA']
+        loop.create_task(parse_from_TA(idx, rest_id, url))    # all the tasks are scheduled at the moment but not all done
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main(loop))
-loop.run_until_complete(asyncio.gather(*asyncio.all_tasks(loop=loop)))  # Wait for all tasks in the loop.
-
-# print(parsed_data)
-parsed_data.to_csv('parsed_data_output.csv')
+for i in range(urls.shape[0] // 10000):    # processing only by 10000 tasks at a time, writing results in files
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop, start=i * 10000, end=(i+1) * 10000))
+    loop.run_until_complete(asyncio.gather(*asyncio.all_tasks(loop=loop)))  # Wait for all tasks in the loop.
+    parsed_data.to_csv(f'parsed_data_output{i}.csv')
