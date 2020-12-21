37524230c5628e79eb31906cec6692f6f1fed3d3
Anastasia Walrave <nbekareva@gmail.com>
2020-12-22 02:27:01 +0700

    scraping pages entirely, using them locaaly

--- module_3/data_scraping.py
+++ module_3/data_scraping.py
@@ -1,12 +1,15 @@
+# scraping from the site straight
+
 import pandas as pd
 from lxml import html
 import asyncio
 import aiohttp
+import json
 
 
 urls = pd.read_csv('main_task.csv', usecols=['Restaurant_id', 'URL_TA'])
 
-parsed_data = pd.DataFrame(columns=['Restaurant_id', 'Cuisine Style', 'Price Range', 'Number of Reviews', 'URL_TA'])
+parsed_dict = {}
 
 semaphore = asyncio.Queue(maxsize=1000)
 
@@ -37,9 +40,14 @@ async def parse_from_TA(idx, rest_id, url):
                 smart_strings=False)  # number of reviews
             current_num = int(current_num[0].replace(',', '')) if len(current_num) > 0 else None
 
-    parsed_data.loc[idx, ['Restaurant_id', 'Cuisine Style', 'Price Range', 'Number of Reviews', 'URL_TA']] = [
-        rest_id, current_cuisines or None, current_price_range, current_num, url
-    ]
+            parsed_dict[idx] = {
+                'Restaurant_id': rest_id,
+                'Cuisine Style': current_cuisines or None,
+                'Price Range': current_price_range,
+                'Number of Reviews': current_num,
+                'URL_TA': url
+            }
+
     await semaphore.get()
 
 
@@ -53,8 +61,10 @@ async def main(loop, start=0, end=urls.shape[0]):
         loop.create_task(parse_from_TA(idx, rest_id, url))    # all the tasks are scheduled at the moment but not all done
 
 
-for i in range(urls.shape[0]//10000):    # processing only by 10000 tasks at a time, writing results in files
+for i in range(urls.shape[0]//10000):    # processing only by 10000 tasks at a time, writing results in files, for safety
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(loop, start=i*10000, end=(i+1)*10000))
     loop.run_until_complete(asyncio.gather(*asyncio.all_tasks(loop=loop)))  # Wait for all tasks in the loop.
-    parsed_data.to_csv(f'parsed_data_output{i}.csv')
+
+    with open(f'parsed_data_output{i}.json', 'w') as outfile:
+        json.dump(parsed_dict, outfile)
